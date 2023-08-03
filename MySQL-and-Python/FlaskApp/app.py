429e4891dedbea2318564832ed11d3a22f8342e0
amrabunemr98 <128842547+amrabunemr98@users.noreply.github.com>
2023-08-03 21:06:04 +0300

    Update app.py

--- MySQL-and-Python/FlaskApp/app.py
+++ MySQL-and-Python/FlaskApp/app.py
@@ -14,6 +14,7 @@ app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
 app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
 app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+app.config['MYSQL_DATABASE_PORT'] = 3306 
 
 mysql.init_app(app)
 
