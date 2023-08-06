8371af6d342f736e48173c3220cad5c25e11bcff
amrabunemr98 <128842547+amrabunemr98@users.noreply.github.com>
2023-08-06 03:28:10 +0300

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,12 +1,13 @@
+# Define an output variable for the public IP of the Jenkins EC2 instance
 output "Public-ip-jenkins" {
-  value = aws_instance.ec2.public_ip
+  value = aws_instance.ec2.public_ip   # Output the public IP of the EC2 instance
 }
 
+# Define an output variable for the ECR repository URL
 output "ecr-url" {
-    value = aws_ecr_repository.sprints_ecr.repository_url
+  value = aws_ecr_repository.sprints_ecr.repository_url   # Output the repository URL
 }
 
-output "ebs-url" {
-    value = aws_ebs_volume.my_ebs_volume.id
-}
+
+
 
