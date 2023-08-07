8b60245ffccb9155b4a7866b561d8446b45d6bac
Amr Abu-Nemr <128842547+amrabunemr98@users.noreply.github.com>
2023-08-08 01:25:32 +0300

    Update sprints.tf

--- Terraform/sprints.tf
+++ Terraform/sprints.tf
@@ -203,6 +203,22 @@ resource "aws_eks_cluster" "eks" {
   ]
 }
 
+resource "aws_iam_role" "cluster-role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = ["eks.amazonaws.com", "ec2.amazonaws.com"]
+        }
+      }
+    ]
+  })
+}
 
 # Create an IAM role for the EKS master
 resource "aws_iam_role" "master" {
